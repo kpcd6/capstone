04fa061a2b3d1ea77de3f1ff0c5e4132f1650bb6
Harrison-n <hhnvx8@mail.missouri.edu>
2013-10-13 15:37:37 -0500

    update register model

--- UAVDefense/src/functions/RegisterModel.java
+++ UAVDefense/src/functions/RegisterModel.java
@@ -2,7 +2,6 @@ package functions;
 
 import java.security.NoSuchAlgorithmException;
 
-import gui.RegisterFrame;
 import database.RegisterDAO;
 
 public class RegisterModel {
@@ -10,11 +9,7 @@ public class RegisterModel {
 	public RegisterModel(RegisterDAO registerDao){
 		this.registerDao = registerDao;
 	}
-	public void attemptLogin(String username, char[] password, char[] confirmPassword, String email) throws NoSuchAlgorithmException {
+	public void attemptRegister(String username, char[] password, char[] confirmPassword, String email) throws NoSuchAlgorithmException {
 		registerDao.attemptRegister(username, password, confirmPassword, email); 
-	}
-	public void attemptRegister()
-	{
-		RegisterFrame frame = new RegisterFrame(); 
 	}
 }
